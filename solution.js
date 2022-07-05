48856c2763c270ba6c3905f19b10f3e303129f2f
MenaMema <amenabarrera@gmail.com>
2022-07-05 18:02:25 +0200

    finished

--- solution.js
+++ solution.js
@@ -1,1 +1,14 @@
+const text = "Hello";
+console.log(isNaN(text));
 
+console.log(isNaN(3));
+
+//Q2
+
+console.log((0.1 * 0.2).toFixed(2));
+
+//Q3
+
+console.log(Infinity / 0);
+console.log(Infinity / Infinity);
+console.log(1 / 0);
